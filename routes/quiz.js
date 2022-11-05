a9b8ad658a39d2fdd923fc4420ce16ebf483a2f2
mk1499 <m.khaled1499@gmail.com>
2022-11-05 23:00:03 +0200

    quiz route

---
+++ routes/quiz.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import Quiz from '../models/quiz';
+
+const quizRouter = express.Router();
+
+quizRouter.get('/',(req,res)=> {
+    
+}); 
+
+export default quizRouter;
