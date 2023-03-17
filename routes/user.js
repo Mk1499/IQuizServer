5404214ceb8b0c1270904f12a7bf106efacc3176
mk1499 <m.khaled1499@gmail.com>
2023-03-17 04:29:46 +0200

    fix code undefined in forgot password

--- routes/user.js
+++ routes/user.js
@@ -164,10 +164,11 @@ userRouter.post('/verifyEmail', authorization, async (req, res) => {
 
 userRouter.post('/forgotPassword', (req, res) => {
   const email = req.body?.email;
-  User.find({ email })
+  User.findOne({ email })
     .then((userData) => {
+      console.log(('user : ', userData.code));
       if (userData) {
-        sendEmail(email, userData.code);
+        // sendEmail(email, userData.code);
       }
       res.status(200).json({ message: 'Code Sent' });
     })
